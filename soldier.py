7e978a21e0cfceec7f26028f627ee9dfecb370e5
Khoi Doan <zombify.25@gmail.com>
2017-02-21 13:05:43 -0500

    More soldiers stuff and weapon class

--- soldier.py
+++ soldier.py
@@ -1,9 +1,19 @@
+class Weapon:
+    def __init__(self, name, minDamage, maxDamage, magSize, rangeMod):
+        self.minDamage = minDamage
+        self.maxDamage = maxDamage
+        self.magSize = magSize
+        self.ammo = magSize
+        self.rangeMod = rangeMod
+
 class Soldier:
-    def __init__(self, coords=(0,0), mobility=5, health = 1000, aim=65):
+    def __init__(self, name, weapon, coords=(0,0), mobility = 5, health = 5, aim = 65):
+        self.name = name
+        self.weapon = weapon
         self.coords = coords
         self.health = health
         self.mobility = mobility
         self.aim = aim
-        
+
     def set_image(self, image):
         self.image = image
