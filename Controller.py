5643bad14e01651d0091f9ca45887ed424be710b
Khoi Doan <zombify.25@gmail.com>
2017-02-22 20:59:26 -0500

    more Controller logic for actions.

--- Controller.py
+++ Controller.py
@@ -1,24 +1,104 @@
 import board
 import math
 import copy
+import random
 
 class Controller:
     def __init__(self):
         self.nothing = "nothing"
-    
+
+    # probably obsolete after more stuff gets added
     def makemove(self, board,mouse1, mouse2):
         coord1 = []
         coord1.append(int(math.floor(mouse1[0]/60)))
         coord1.append(int(math.floor(mouse1[1]/60)))
-        
-        
+
+
         coord1.append(int(math.floor(mouse2[0])/60))
         coord1.append(int(math.floor(mouse2[1])/60))
-        
+
         if (board.tiles[coord1[0]][coord1[1]].unit != None)&(board.tiles[coord1[2]][coord1[3]].unit != None):
-            
+
             dummyUnit = copy.copy(board.tiles[coord1[0]][coord1[1]].unit)
             board.tiles[coord1[0]][coord1[1]].unit = copy.copy(board.tiles[coord1[2]][coord1[3]].unit)
             board.tiles[coord1[2]][coord1[3]].unit = dummyUnit
-        
+
         return board
+
+    def getTile(self, board, mouse):
+        coord1 = []
+        coord1.append(int(math.floor(mouse[0]/60)))
+        coord1.append(int(math.floor(mouse[1]/60)))
+        return board.tiles[coord1[0]][coord1[1]]
+
+    # id = 1: move (1 action)
+    # id = 2: dash (2 actions)
+    # id = 3: shoot (1 action, turn-ending)
+    # id = 4: reload (1 action)
+    def possibleAction(self, srcTile):
+        l = []
+        # can't act if dead or no moves left
+        if srcTile.unit.health <= 0 or srcTile.unit.actionPoints <= 0:
+            return l
+        l.append(1)
+        if srcTile.unit.actionPoints == 2:
+            l.append(2)
+        if srcTile.unit.weapon.ammo > 0:
+            l.append(3)
+        if srcTile.unit.weapon.ammo < srcTile.unit.weapon.magSize:
+            l.append(4)
+        return l
+
+    # possible target tiles for action
+    def possibleTiles(self, board, srcTile, ID):
+        l = []
+        if ID == 1:
+            cameFrom, cost_so_far = board.moveDistance(srcTile, srcTile.unit.mobility)
+            for t in cost_so_far:
+                l.append(t)
+        if ID == 2:
+            cameFrom, cost_so_far = board.moveDistance(srcTile, srcTile.unit.mobility * 2)
+            for t in cost_so_far:
+                l.append(t)
+        if ID == 3:
+            for i in range(0, board.width):
+                for t in board.tiles[i]:
+                    if t.unit != None and t.unit.side != srcTile.unit.side:
+                        l.append(t)
+        if ID == 4:
+            l.append(srcTile)
+        return l
+
+    # this presumes that possibleTiles and possibleActions are valid, and makes no checking attempt
+    # updates board, returns a message indicating outcome
+    def performAction(self, board, srcTile, desTile, ID):
+        msg = ""
+        if ID == 1 or ID == 2:
+            srcTile.unit.actionPoints -= ID
+            desTile.unit = srcTile.unit
+            srcTile.passable = True
+            desTile.passable = False
+        if ID == 3:
+            srcTile.unit.actionPoints = 0
+            hitChance = srcTile.unit.aim
+            cover = board.coverValue(srcTile, desTile)
+            if (cover <= 0): # flanking gets a 40% bonus
+                hitChance += 40
+            else: # if not flanking then aiming chance reduced by cover
+                hitChance -= cover
+            # range modifiers; weapons do better at close range and worse faraway
+            hitChance += srcTile.unit.weapon.rangeMod[board.straightDistance(srcTile, desTile)]
+            # RNGESUS COMETH
+            if randint(0, 100) <= hitChance:
+                damage = randint(srcTile.unit.weapon.minDamage, srcTile.unit.weapon.maxDamage)
+                desTile.unit.health -= damage
+                srcTile.unit.weapon.ammo -= 1
+                msg = "Hit: " + str(hitChance) + "% chance, " + str(damage) + " damage dealt."
+            else:
+                msg = "Missed: " + str(hitChance) + "% chance."
+        if ID == 4:
+            srcTile.unit.actionPoints -= 1
+            srcTile.unit.weapon.ammo = srcTile.unit.weapon.magSize
+            msg = "Weapon reloaded."
+        return msg
+
--- soldier.py
+++ soldier.py
@@ -7,13 +7,15 @@ class Weapon:
         self.rangeMod = rangeMod
 
 class Soldier:
-    def __init__(self, name, weapon, coords=(0,0), mobility = 5, health = 5, aim = 65):
+    def __init__(self, name, weapon, coords=(0,0), mobility = 5, health = 5, aim = 65, side = 0):
         self.name = name
         self.weapon = weapon
         self.coords = coords
         self.health = health
         self.mobility = mobility
         self.aim = aim
+        self.side = side
+        self.actionPoints = 2
 
     def set_image(self, image):
         self.image = image
