f569e69bf8383205bf9caae1f02e61057c7d5836
Khoi Doan <zombify.25@gmail.com>
2017-02-26 15:12:32 -0500

    Cover alpha color depends on actual cover value

--- Renderer.py
+++ Renderer.py
@@ -16,10 +16,10 @@ main_dir = os.path.split(os.path.abspath(__file__))[0]
 
 class Renderer():
     def __init__(self,board,screen):
-        
+
         self.board = board
         self.screen = screen
-        
+
 
         #Create The Backgound
         self.background = pygame.Surface(self.screen.get_size())
@@ -29,7 +29,7 @@ class Renderer():
         self.papixel = helper.load_image('papixel.png').convert()
         transColor = self.papixel.get_at((0,0))
         self.papixel.set_colorkey(transColor)
-        
+
         #add character to background
         self.background.blit(self.papixel,(500,500))
 
@@ -47,7 +47,7 @@ class Renderer():
         papixel = pygame.Surface((helper.getResolution(),helper.getResolution()))
         papixel = papixel.convert()
         papixel.fill((255, 255, 255))
-        
+
         wall = helper.load_image('wall.png').convert()
         transColor = wall.get_at((0,0))
         wall.set_colorkey(transColor)
@@ -64,41 +64,41 @@ class Renderer():
                 #pygame.draw.rect(blank, (50,140,200), (0,0,60,60), 2)
                 res = helper.getResolution()
                 if (self.board.tiles[i][j].coverN != 0):
-                    pygame.draw.line(blank, (50,140,200), (0,0), (res,0), 4)
-                    
+                    pygame.draw.line(blank, (50,140,200,self.board.tiles[i][j].coverN), (0,0), (res,0), 4)
+
                 if (self.board.tiles[i][j].coverS != 0):
-                    pygame.draw.line(blank, (50,140,200), (0,res+2), (res,res+2), 4)
-                    
+                    pygame.draw.line(blank, (50,140,200,self.board.tiles[i][j].coverS), (0,res+2), (res,res+2), 4)
+
                 if (self.board.tiles[i][j].coverW != 0):
-                    pygame.draw.line(blank, (50,140,200), (0,0), (0,res+2), 4)
-                    
+                    pygame.draw.line(blank, (50,140,200,self.board.tiles[i][j].coverW), (0,0), (0,res+2), 4)
+
                 if (self.board.tiles[i][j].coverE != 0):
-                    pygame.draw.line(blank, (50,140,200), (res+2,0), (res+2,res), 4)
-                    
-                    
+                    pygame.draw.line(blank, (50,140,200,self.board.tiles[i][j].coverE), (res+2,0), (res+2,res), 4)
+
+
         #self.coverN = coverN
         #self.coverE = coverE
         #self.coverS = coverS
         #self.coverW = coverW
-                
+
                 if (self.board.tiles[i][j].unit != None):
                     blank.blit(self.board.tiles[i][j].unit.image, (2,2))
                     #test = 1
                 elif(self.board.tiles[i][j].passable == False):
                     blank.blit(wall, (2,2))
-                else:    
+                else:
                     blank.blit(papixel, (2,2))
-                    
+
                 dummy.blit(blank, (i*(res+4),j*(res+4)))
 
-        
+
         #Display The Background
-        
+
         self.screen.blit(dummy, (0, 0))
         self.background = dummy
-        
+
         pygame.display.flip()
-    
+
     def renderPossibleTiles(self,possibleTiles):
         self.render(self.board)
         #print("possibleTile")
@@ -107,30 +107,30 @@ class Renderer():
         dummy.fill((255,255,255))
         dummy.set_alpha(75)
         res = helper.getResolution()
-        for tile in possibleTiles: 
+        for tile in possibleTiles:
             cover = pygame.Surface((res,res))
             cover = cover.convert()
             cover.fill((0, 140, 255))
             cover.set_alpha(75)
             dummy.blit(cover, (tile.coords[0]*(res+4),tile.coords[1]*(res+4)))
-            
+
         self.background.blit(dummy, (0,0))
         self.screen.blit(self.background, (0, 0))
         pygame.display.flip()
-            
+
     def renderHover(self, tile, myfont):
-        
+
         dummy = pygame.Surface(self.screen.get_size())
         dummy = dummy.convert()
         dummy.fill((255,255,255))
         dummy.set_alpha(75)
         res = helper.getResolution()
-        
+
         hover = pygame.Surface(((res+4)*3,(res+4)*3))
         hover = hover.convert()
         hover.fill((140, 255, 255))
         #hover.set_alpha(75)
-        
+
         unit = tile.unit
 #         self.health = health
 #         self.mobility = mobility
@@ -159,16 +159,16 @@ class Renderer():
         ypos = 0
         if (tile.coords[0] < self.board.width/2):
             xpos = res
-        else: 
+        else:
             xpos = - 3*res
         if (tile.coords[1] < self.board.height/2):
             ypos = res
-        else: 
+        else:
             ypos = - 3*res
-        
+
         self.background.blit(hover, (tile.coords[0]*(res+4)+xpos,tile.coords[1]*(res+4)+ypos))
         #self.background.blit(dummy, (0,0))
         self.screen.blit(self.background, (0, 0))
         pygame.display.flip()
-        
-            
+
+
